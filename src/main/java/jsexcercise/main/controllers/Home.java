604b83d084a100f4832ce303e87e0974d25e4dd0
PatrickVincentHoestroem <patrick.vincent@live.dk>
2020-12-21 16:29:39 +0100

    Initial commit

---
+++ src/main/java/jsexcercise/main/controllers/Home.java
@@ -0,0 +1,14 @@
+package jsexcercise.main.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Home {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+}
